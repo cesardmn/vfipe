4b5f27bbc5bf01bddc5eb72992a17783250900b5
cesardmn <cesardmn@gmail.com>
2024-10-01 01:38:00 -0300

    feat: implementando paginação no componente Vehicle.

--- src/app/components/Vehicles/index.jsx
+++ src/app/components/Vehicles/index.jsx
@@ -2,77 +2,119 @@ import { useStep } from "@/app/providers/StepProvider";
 import { fetchData } from "@/services/FetchData";
 import { useEffect, useState } from "react";
 import Skeleton from "../Skeleton";
-
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 const Vehicles = () => {
   const { step } = useStep();
   const [vehicles, setVehicles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const vehiclesPerPage = 5;
+  const [years, setYears] = useState([]);
 
-  const handleVehicles = async () => {
+  const fetchVehiclesForPage = async (page) => {
     const { refId, typeId, brandId, modelId } = step;
     setLoading(true);
-  
+
     try {
-      const years = await fetchData(`/api/anomodelo/${refId}/${typeId}/${brandId}/${modelId}`);
-      
-      const vehiclesData = await years.reduce(async (acc, year) => {
-        const accumulatedVehicles = await acc; // Aguarda o acumulador
+      const startYearIndex = (page - 1) * vehiclesPerPage;
+      const endYearIndex = startYearIndex + vehiclesPerPage;
+
+
+      const fetchedYears = years.slice(startYearIndex, endYearIndex);
+
+      const vehiclesData = await Promise.all(fetchedYears.map(async (year) => {
         const url = `/api/vehicle/${refId}/${typeId}/${brandId}/${modelId}/${year.id}`;
-        
         try {
-          const vehicleData = await fetchData(url);
-          return [...accumulatedVehicles, vehicleData]; // Adiciona o veículo ao acumulador
+          return await fetchData(url);
         } catch (error) {
           console.error(`Error fetching vehicle for year ${year.id}:`, error);
-          return accumulatedVehicles; // Retorna o acumulador mesmo em caso de erro
+          return null;
         }
-  
-      }, Promise.resolve([])); // Começa com uma promessa resolvida que contém um array vazio
-  
-      setVehicles(vehiclesData);
+      }));
+
+
+      const validVehicles = vehiclesData.filter(vehicle => vehicle !== null);
+      setVehicles(validVehicles);
     } catch (error) {
       console.error("Error fetching vehicles:", error);
     } finally {
       setLoading(false);
     }
   };
-  
+
+  const handleYears = async () => {
+    const { refId, typeId, brandId, modelId } = step;
+
+    try {
+      const fetchedYears = await fetchData(`/api/anomodelo/${refId}/${typeId}/${brandId}/${modelId}`);
+      setYears(fetchedYears);
+
+      fetchVehiclesForPage(1);
+    } catch (error) {
+      console.error("Error fetching years:", error);
+    }
+  };
+
   useEffect(() => {
     if (step.refId && step.typeId && step.brandId && step.modelId) {
-      handleVehicles();
+      handleYears();
     }
   }, [step]);
+
+  useEffect(() => {
+    if (years.length > 0) {
+      fetchVehiclesForPage(currentPage);
+    }
+  }, [currentPage, years]);
+
+  const totalPages = Math.ceil(years.length / vehiclesPerPage);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
   if (loading) {
     return <Skeleton />;
   }
 
   return (
-    <div className={styles.tableContainer} >
-      <table className={styles.table} >
-        <caption>
-          {`Código FIPE: ${(vehicles.length > 0) && vehicles[0].fipe}`}
-        </caption>
-        <thead>
-          <tr>
-            <th>ano</th>
-            <th>combustível</th>
-            <th>valor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {vehicles.map(vehicle => (
-            <tr key={vehicle.authentication}>
-              <td>{vehicle.year == '32000' ? 'Zero Km' : vehicle.year}</td>
-              <td>{vehicle.fuel}</td>
-              <td>{vehicle.price}</td>
+    <>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <caption>
+            {`Código FIPE: ${(vehicles.length > 0) && vehicles[0].fipe}`}
+          </caption>
+          <thead>
+            <tr>
+              <th>ano</th>
+              <th>combustível</th>
+              <th>valor</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {vehicles.map(vehicle => (
+              <tr key={vehicle.authentication}>
+                <td>{vehicle.year == '32000' ? 'Zero Km' : vehicle.year}</td>
+                <td>{vehicle.fuel}</td>
+                <td>{vehicle.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.pagination}>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? styles.active : ''}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
 
