fc284183228e9d5023d9ff6677f006425b8ce099
cesardmn <cesardmn@gmail.com>
2025-05-16 12:49:33 -0300

    feat(router): redirect main page to new route.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,16 @@
-import styles from './page.module.css'
-import Header from '@/app/components/Header'
+'use client';
+
+import Header from '@/app/components/Header';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Skeleton from './components/Skeleton';
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main>main</main>
-    </>
-  )
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/consulta');
+  }, [router]);
+
+  return <Skeleton />;
 }
