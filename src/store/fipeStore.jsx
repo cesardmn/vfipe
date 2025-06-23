01eb76953436d34d01a1e7015ac7d1eaafe3822d
cesardmn <cesardmn@gmail.com>
2025-06-22 23:06:15 -0300

    refactor: Substituindo context  providers de por zustand store.

--- src/store/fipeStore.jsx
+++ src/store/fipeStore.jsx
@@ -2,19 +2,38 @@ import { create } from 'zustand';
 
 export const useFipe = create((set) => ({
   referenceTableList: [],
-  setreferenceTableList: (reference) => set({ referenceTableList: reference }),
-  
+  setReferenceTableList: (reference) => set({ referenceTableList: reference }),
+
   refId: '',
   setRefId: (id) => set({ refId: id }),
-  
-  typeId: '',
-  setTypeId: (id) => set({ typeId: id }),
-  
+
   type: {},
   setType: (obj) => set({ type: obj }),
-  
+
+  typeId: '',
+  setTypeId: (id) => set({ typeId: id }),
+
   brandList: {},
   setBrandList: (obj) => set({ brandList: obj }),
+
+  brandId: '',
+  setBrandId: (id) => set({ brandId: id }),
+
+  modelList: {},
+  setModelList: (obj) => set({ modelList: obj }),
+
+  modelId: '',
+  setModelId: (id) => set({ modelId: id }),
+
+  modelYearsList: {},
+  setModelYearsList: (obj) => set({ modelYearsList: obj }),
+
+  modelYearsId: '',
+  setModelYearsId: (id) => set({ modelYearsId: id }),
+
+
+  resultShow: '',
+  setResultShow: (id) => set({ resultShow: id }),
 
 }));
 
